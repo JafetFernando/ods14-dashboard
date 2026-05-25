cb305563a610c6a934cf441a1589d9af50fb23fd
JafetFernando <131724324+JafetFernando@users.noreply.github.com>
2026-05-25 13:00:06 -0600

    Update app_dashboard.py

--- app_dashboard.py
+++ app_dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import zipfile
 
 # Configuración de la página (Debe ir al inicio)
 st.set_page_config(page_title="Vigilancia Marítima - ODS 14", layout="wide")
